ef5ab28ee8e3155218fb944f50335c395da3df7e
Sara1703 <sara.julissa.hernandez@correounivalle.edu.co>
2025-05-06 00:50:03 -0500

    se agregó huella biométrica

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.biometric.ktx)
     val navVersion = "2.3.5"
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)

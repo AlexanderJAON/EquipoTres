3f4cf9fec49ee395dabd5c5ebdc4874ed7b98501
Santiago-AGZ <santiago.alvarez.gutierrez@correounivalle.edu.co>
2025-05-12 20:59:45 -0500

    Add endpoints for dog breeds and images
    This commit adds two new endpoints to the ApiService interface:
    - `getAllBreeds()`: Retrieves a list of all dog breeds.
    - `getRandomImageByBreed(breed: String)`: Retrieves a random image for a specific dog breed.
    
    It also includes the necessary data classes (`DogBreedsResponse` and `DogImageResponse`) to handle the responses from these endpoints.

--- app/src/main/java/com/dogAPPackage/dogapp/webservice/ApiService.kt
+++ app/src/main/java/com/dogAPPackage/dogapp/webservice/ApiService.kt
@@ -1,9 +1,18 @@
 package com.dogAPPackage.dogapp.webservice
 import com.dogAPPackage.dogapp.model.AppointmentModelResponse
+import com.dogAPPackage.dogapp.model.DogBreedsResponse
 import com.dogAPPackage.dogapp.utils.Constants.END_POINT
+import com.dogAPPackage.dogapp.model.DogImageResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET(END_POINT)
     suspend fun getProducts(): MutableList<AppointmentModelResponse>
+
+    @GET("breeds/list/all")
+    suspend fun getAllBreeds(): DogBreedsResponse
+
+    @GET("breed/{breed}/images/random")
+    suspend fun getRandomImageByBreed(@Path("breed") breed: String): DogImageResponse
 }

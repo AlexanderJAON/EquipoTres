2a9303044009df134a9b2e0c22132c80f70b63a5
Camilo Arboleda <arboledacamilo051@gmail.com>
2025-05-14 13:10:03 -0500

    Creacion de validaciones en los campos, consumo de Api y guardado en la base de datos

--- app/src/main/java/com/dogAPPackage/dogapp/view/fragment/AddAppointmentFragment.kt
+++ app/src/main/java/com/dogAPPackage/dogapp/view/fragment/AddAppointmentFragment.kt
@@ -10,6 +10,16 @@ import androidx.fragment.app.viewModels
 import com.dogAPPackage.dogapp.databinding.FragmentNuevaCitaBinding
 import com.dogAPPackage.dogapp.model.Appointment
 import com.dogAPPackage.dogapp.viewmodel.AppointmentViewModel
+import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
+import com.dogAPPackage.dogapp.R
+import android.text.TextWatcher
+import android.text.Editable
+import android.graphics.Typeface
+import androidx.core.content.ContextCompat
+import android.text.InputFilter
+
+
 
 class AddAppointmentFragment : Fragment() {
 
@@ -28,10 +38,69 @@ class AddAppointmentFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.etNombreMascota.filters = arrayOf(InputFilter.LengthFilter(15))
+        binding.actvRaza.filters = arrayOf(InputFilter.LengthFilter(20))
+        binding.etNombrePropietario.filters = arrayOf(InputFilter.LengthFilter(30))
+        binding.etTelefono.filters = arrayOf(InputFilter.LengthFilter(10))
+        viewModel.getBreedsFromApi()
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_addAppointmentFragment_to_homeFragment)
+        }
+
+        viewModel.breedsList.observe(viewLifecycleOwner) { breeds ->
+            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, breeds)
+            binding.actvRaza.setAdapter(adapter)
+        }
+
+        val sintomas = resources.getStringArray(R.array.sintomas)
+        val sintomasAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, sintomas)
+        binding.spinnerSintomas.setAdapter(sintomasAdapter)
+        binding.spinnerSintomas.setText(sintomas[0], false)
+
+        viewModel.imageUrl.observe(viewLifecycleOwner) { imageUrl -> }
+
+        binding.etNombreMascota.addTextChangedListener(validationTextWatcher)
+        binding.actvRaza.addTextChangedListener(validationTextWatcher)
+        binding.etNombrePropietario.addTextChangedListener(validationTextWatcher)
+        binding.etTelefono.addTextChangedListener(validationTextWatcher)
+        binding.spinnerSintomas.addTextChangedListener(validationTextWatcher)
+        checkFields()
         binding.btnGuardar.setOnClickListener {
             guardarCita()
         }
+    }
 
+    private val validationTextWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            checkFields()
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+    }
+
+    private fun checkFields() {
+        val petName = binding.etNombreMascota.text.toString()
+        val breed = binding.actvRaza.text.toString()
+        val ownerName = binding.etNombrePropietario.text.toString()
+        val phone = binding.etTelefono.text.toString()
+        val symptom = binding.spinnerSintomas.text.toString()
+
+        binding.btnGuardar.isEnabled = petName.isNotBlank() && breed.isNotBlank() &&
+                ownerName.isNotBlank() && phone.isNotBlank() &&
+                symptom != "Síntomas"
+
+        if (binding.btnGuardar.isEnabled) {
+            binding.btnGuardar.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.pink))
+            binding.btnGuardar.setTextColor(ContextCompat.getColor(requireContext(), android.R.color.white))
+            binding.btnGuardar.setTypeface(null, Typeface.BOLD)
+        } else {
+            binding.btnGuardar.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.pink))
+            binding.btnGuardar.setTextColor(ContextCompat.getColor(requireContext(), android.R.color.darker_gray))
+            binding.btnGuardar.setTypeface(null, Typeface.NORMAL)
+        }
     }
 
     private fun guardarCita() {
@@ -40,24 +109,36 @@ class AddAppointmentFragment : Fragment() {
         val ownerName = binding.etNombrePropietario.text.toString()
         val phone = binding.etTelefono.text.toString()
         val symptom = binding.spinnerSintomas.text.toString()
-        val imageUrl = "" // Aquí puedes poner una URL fija o agregar lógica para subir imagen
 
-        if (petName.isBlank() || breed.isBlank() || ownerName.isBlank() || phone.isBlank() ) {
-            Toast.makeText(requireContext(), "Completa todos los campos", Toast.LENGTH_SHORT).show()
+        if (symptom.isEmpty() || symptom == "Selecciona un síntoma") {
+            Toast.makeText(requireContext(), "Selecciona un síntoma", Toast.LENGTH_SHORT).show()
             return
         }
 
-        val appointment = Appointment(
-            petName = petName,
-            breed = breed,
-            ownerName = ownerName,
-            phone = phone,
-            symptom = symptom,
-            imageUrl = imageUrl
-        )
+        viewModel.getRandomImageByBreed(breed)
 
-        viewModel.saveAppointment(appointment)
-        Toast.makeText(requireContext(), "Cita guardada con éxito", Toast.LENGTH_SHORT).show()
+        viewModel.imageUrl.observe(viewLifecycleOwner) { imageUrl ->
+            val imageUrl = imageUrl ?: ""
+
+            if (petName.isBlank() || breed.isBlank() || ownerName.isBlank() || phone.isBlank() || symptom == "Síntomas") {
+                Toast.makeText(requireContext(), "Completa todos los campos correctamente", Toast.LENGTH_SHORT).show()
+                return@observe
+            }
+
+            val appointment = Appointment(
+                petName = petName,
+                breed = breed,
+                ownerName = ownerName,
+                phone = phone,
+                symptom = symptom,
+                imageUrl = imageUrl
+            )
+
+            viewModel.saveAppointment(appointment)
+
+            Toast.makeText(requireContext(), "Cita guardada con éxito", Toast.LENGTH_SHORT).show()
+
+            findNavController().navigate(R.id.action_addAppointmentFragment_to_homeFragment)
+        }
     }
-
 }
